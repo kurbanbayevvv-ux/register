973e39201dec69a6f72926407d1bb00f429e6979
kurbanbayevvv-ux <ulugbek@kali>
2026-03-18 20:05:06 +0500

    register

--- app/handlers/register.py
+++ app/handlers/register.py
@@ -3,6 +3,12 @@ from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from keyboards.keyboards import keyboard_start, keyboard_phone_number, keyboard_courses
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+isAdmin = lambda x: os.getenv('ADMIN') == str(x)
+
 from database.postgres import Register as RegisterUser, addUserInfo
 
 from states.register import Register
@@ -14,22 +20,20 @@ async def start_handler(message: types.Message):
     chatId = message.chat.id
     firstName = message.chat.first_name
     RegisterUser(chat_id=chatId, first_name=firstName)
-    await message.answer(f"Salom xush kelibsiz! {firstName}", reply_markup=keyboard_start)
-
-# start register
+    await message.answer(f"Salom xush kelibsiz! {firstName}", reply_markup=keyboard_start(isAdmin(chatId)))
+    
 @router.message(lambda msg: msg.text == "Ro'yxatdan o'tish")
 async def reg_start(message: types.Message, state: FSMContext):
     await message.answer("Ismingizni kiriting:")
     await state.set_state(Register.name)
-
-# name
+ 
 @router.message(Register.name)
 async def get_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer("Telefon raqamingiz:", reply_markup=keyboard_phone_number)
     await state.set_state(Register.phone)
 
-# phone
+
 @router.message(Register.phone)
 async def get_phone(message: types.Message, state: FSMContext):
     if message.contact:
@@ -43,7 +47,7 @@ async def get_phone(message: types.Message, state: FSMContext):
     await state.update_data(age=message.text)
     await message.answer("Qaysi kursga yozilmoqchisiz:", reply_markup=keyboard_courses)
     await state.set_state(Register.course)
-# age
+
 @router.message(Register.course)
 async def get_age(message: types.Message, state: FSMContext):
     data = await state.update_data(course=message.text)
